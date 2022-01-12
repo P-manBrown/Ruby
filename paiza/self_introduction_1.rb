c573f16364a38e64be0aa4f0702bcccd9619313a
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-01-12 09:24:23 +0900

    finish paiza self_introduction_1

---
+++ paiza/self_introduction_1.rb
@@ -0,0 +1,5 @@
+# https://paiza.jp/works/mondai/self_introduction/self_introduction__1
+
+puts "name: P-man Brown"
+puts "language: Ruby"
+puts "hitokoto: Hello, world!"
---
+++ stdin_1.rb
@@ -0,0 +1,4 @@
+# https://paiza.jp/works/mondai/stdin/stdin_1
+
+input_line = gets
+puts input_line
